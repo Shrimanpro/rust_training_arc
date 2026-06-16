8dc5eec79d1fbfdaaea1fdc63f8a9719fa6a9598
Shriman <shrimanpro.gamer@gmail.com>
2026-06-16 03:22:23 -0700

    testing out Option<>

--- cli_calculator/src/main.rs
+++ cli_calculator/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    let mut temp: String = String::new();
+    let _ = std::io::stdin().read_line(&mut temp);
+    let mut t = temp.split_whitespace();
+    let s = t.next();
+    // println!("{}", s);
 }
