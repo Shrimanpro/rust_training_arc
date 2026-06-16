e00f9966aea41cd66daee1a29c7c19d0d21c72a7
Shriman <shrimanpro.gamer@gmail.com>
2026-06-16 04:07:26 -0700

    if let = match

--- cli_calculator/src/main.rs
+++ cli_calculator/src/main.rs
@@ -4,5 +4,15 @@ fn main() {
     let _ = std::io::stdin().read_line(&mut temp);
     let mut t = temp.split_whitespace();
     let s = t.next();
+
+    if let Some(val) = s 
+    {
+        println!("{}", val);
+
+    }
+    else 
+    {
+        println!("er");
+    }
     // println!("{}", s);
 }
